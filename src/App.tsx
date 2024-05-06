9119ca0962e3e4665301695820a4a3f5bff4d569
ojela028xamk <jerelans91@gmail.com>
2024-05-06 13:37:33 +0300

    prettier format to files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import './App.css'
+import css from './App.module.scss'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className={css.App}>
+      <h1>Top News</h1>
+    </div>
+  )
 }
 
 export default App
