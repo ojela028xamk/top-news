27fbb2a486463a5e9308d0ea437357a8995afbf8
ojela028xamk <jerelans91@gmail.com>
2024-05-11 13:29:22 +0300

    implement top headlines endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { useEffectOnce } from 'react-use'
 import css from './App.module.scss'
+// import { getTopHeadlines } from './Services/topHeadlineService'
 
 function App() {
+  useEffectOnce(() => {
+    console.log('endpoint disabled')
+    /*getTopHeadlines()
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err))*/
+  })
+
   return (
     <div className={css.App}>
       <h1>Top News</h1>
