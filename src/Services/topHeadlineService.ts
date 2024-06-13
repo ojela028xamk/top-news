905d8be9f2abf19d3f47be1dd8b2ba1f35878f18
ojela028xamk <jerelans91@gmail.com>
2024-06-13 12:30:49 +0300

    world news api

--- src/Services/topHeadlineService.ts
+++ src/Services/topHeadlineService.ts
@@ -2,7 +2,7 @@ import { Categories } from '../globalTypes'
 
 const token = process.env.REACT_APP_NEWSAPI
 
-const getNews = async (url: string) => {
+const getNewsLegacy = async (url: string) => {
   const options = {
     method: 'GET',
     headers: {
@@ -19,9 +19,29 @@ const getNews = async (url: string) => {
 }
 
 const getTopHeadlines = async (category: Categories) => {
-  return getNews(
+  return getNewsLegacy(
     `https://newsapi.org/v2/top-headlines?country=us&apiKey=${token}&category=${category.toLocaleLowerCase()}`
   )
 }
 
-export { getTopHeadlines }
+const getNews = async (url: string) => {
+  const options = {
+    method: 'GET',
+    headers: {
+      'x-api-key': '7627a5aeee3b4341a014c28f5d138401',
+    },
+  }
+
+  try {
+    const response = await fetch(url, options)
+    return await response.json()
+  } catch (err) {
+    throw err
+  }
+}
+
+const getTopNews = async () => {
+  return getNews(`https://api.worldnewsapi.com/top-news?source-country=us&language=en&date=2024-06-12`)
+}
+
+export { getTopHeadlines, getTopNews }
--- src/globalTypes.ts
+++ src/globalTypes.ts
@@ -36,3 +36,29 @@ export type Source = {
   id: string
   name: string
 }
+
+export type TopNewsResponse = {
+  top_news: TopNews[]
+  language: string
+  country: string
+}
+
+export type TopNews = {
+  news: News[]
+}
+
+export type News = {
+  id: number
+  title: string
+  text: string
+  summary: string
+  url: string
+  image: string
+  video: string
+  publish_date: string
+  author: string
+  authors: string[]
+  language: string
+  source_country: string
+  sentiment: number
+}
